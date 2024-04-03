6f04eb76a98dc869b7c9e607ca76c1202c68c6d9
Paberu <paberu.tocador@gmail.com>
2024-04-03 06:38:12 +0300

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -4,43 +4,30 @@ import random
 
 class PasswordGenerator:
 
-    types = {
-        1: string.digits,
-        2: string.ascii_lowercase,
-        3: string.ascii_uppercase,
-        4: string.ascii_letters,
-        5: string.ascii_letters + string.digits,
-        6: string.ascii_letters + string.digits + string.punctuation
-    }
-
-    def __init__(self, length, password_type):
-        self.length = length
-        self.password_type = password_type
+    def __init__(self, size):
+        self.size = size
 
     def generate_password(self):
-        password_symbols = PasswordGenerator.types[self.password_type]
-        random_password = ''.join(random.choice(password_symbols) for _ in range(self.length))
-        return random_password
+        specific_symbols_count = self.size // 6 + 1
+        digits_count = self.size // 4
+        uppercase_count = (self.size - specific_symbols_count - digits_count) // 2
+        lowercase_count = self.size - uppercase_count - specific_symbols_count - digits_count
+
+        password_symbols = [random.choice(string.punctuation) for _ in range(specific_symbols_count)]
+        password_symbols.extend([random.choice(string.digits) for _ in range(digits_count)])
+        password_symbols.extend([random.choice(string.ascii_uppercase) for _ in range(uppercase_count)])
+        password_symbols.extend([random.choice(string.ascii_lowercase) for _ in range(lowercase_count)])
+
+        random.shuffle(password_symbols)
+
+        return ''.join(password_symbols)
 
 
 if __name__ == '__main__':
 
-    types = {
-        1: '1. Digits only',
-        2: '2. Lowercase letters only',
-        3: '3. Uppercase letters only',
-        4: '4. Uppercase and lowercase letters',
-        5: '5. Uppercase and lowercase letters and digits',
-        6: '6. Uppercase and lowercase letters, digits and special symbols'
-    }
-
-    for password_type in sorted(types.values()):
-        print(password_type)
-
-    password_type = int(input('Choose your password policy: '))
     length = int(input('Print password length: '))
 
-    password_generator = PasswordGenerator(length, password_type)
+    password_generator = PasswordGenerator(length)
     print(password_generator.generate_password())
 
 
